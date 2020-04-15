a9af32c3826a01622c5e6cc8141cb77924ec980b
mrclag <mattrclagett@gmail.com>
2020-04-14 18:38:10 -0700

    django setup for image models, urls

--- src/whatimg/settings.py
+++ src/whatimg/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'images',
+
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media_root')
+MEDIA_URL = '/media/'
