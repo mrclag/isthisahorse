f2e7567b3680dfaee03430cda0247dfb4a0d1d09
mrclag <mattrclagett@gmail.com>
2020-04-28 20:08:35 -0700

    Added drag and drop + click  file upload with react-dropzone

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button } from 'react-bootstrap';
+import Classifier from './components/classifier/Classifier.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <Button variant="primary">Test</Button>
+      <Classifier />
+      {/* <h1>Hello World</h1>
+      <Button variant="primary">Test</Button> */}
     </div>
   );
 }
