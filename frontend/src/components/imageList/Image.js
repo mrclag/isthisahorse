54a900aadc2e45d92a9c16b4b13cf78384a989fe
mrclag <mattrclagett@gmail.com>
2020-04-29 23:04:09 -0700

    Fixed conditional statebased rendering for images/text

--- frontend/src/components/imageList/Image.js
+++ frontend/src/components/imageList/Image.js
@@ -4,7 +4,7 @@ import { Card } from 'react-bootstrap';
 const Image = ({ picture, name }) => {
   return (
     <>
-      <Card className="mx-auto mb-3" style={{ width: '18rem' }}>
+      <Card className="mx-auto mb-3" style={{ width: '24rem' }}>
         <Card.Img variant="top" src={picture} />
         <Card.Body>
           <Card.Title>Classified as: {name}</Card.Title>
--- frontend/src/components/imageList/ImageList.js
+++ frontend/src/components/imageList/ImageList.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import { Button, Spinner } from 'react-bootstrap';
 import Image from './Image';
 import axios from 'axios';
 
 class ImageList extends Component {
   state = {
     images: [],
+    visible: 2,
+    isLoading: true,
+    newLoaded: false,
+    status: false,
   };
 
   componentDidMount() {
-    this.getImages();
+    setTimeout(this.getImages, 500);
   }
 
   getImages = () => {
@@ -19,20 +24,55 @@ class ImageList extends Component {
         },
       })
       .then((res) => {
-        this.setState({ images: res.data });
+        this.setState({ images: res.data, status: true });
         console.log(res);
       });
+    this.setState({ isLoading: false });
+  };
+
+  handleVisible = () => {
+    const visible = this.state.visible;
+    const new_visible = visible + 2;
+    this.setState({ newLoaded: true });
+    setTimeout(() => {
+      this.setState({ visible: new_visible, newLoaded: false });
+    }, 300);
   };
 
   render() {
-    const images = this.state.images.map((img) => {
+    const images = this.state.images.slice(0, this.state.visible).map((img) => {
       return <Image key={img.id} picture={img.picture} name={img.classified} />;
     });
 
     return (
       <>
         <h1>Image List Here</h1>
-        {images}
+        {this.state.isLoading ? (
+          <Spinner animation="border" role="status"></Spinner>
+        ) : (
+          <>
+            {this.state.images.length === 0 && this.state.status && (
+              <h3>No images classified.</h3>
+            )}
+            {images}
+            {this.state.newLoaded && (
+              <Spinner animation="border" role="status"></Spinner>
+            )}
+            <br />
+            {this.state.images.length > this.state.visible &&
+              this.state.images.length > 2 && (
+                <Button
+                  variant="primary"
+                  size="lg"
+                  onClick={this.handleVisible}
+                >
+                  Load More
+                </Button>
+              )}
+            {this.state.images.length <= this.state.visible &&
+              this.state.images.length > 0 && <h3>No more images to load</h3>}
+          </>
+        )}
       </>
     );
   }
